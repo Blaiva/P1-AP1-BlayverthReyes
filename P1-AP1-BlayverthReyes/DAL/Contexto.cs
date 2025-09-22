6ea65f9d8568fa0f354274e2c697bc4db5958424
Blaiva <blayverthreyesp14@gmail.com>
2025-09-22 19:29:28 -0400

    Creacion del contexto

---
+++ P1-AP1-BlayverthReyes/DAL/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P1_AP1_BlayverthReyes.Models;
+
+namespace P1_AP1_BlayverthReyes.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+    public DbSet<Registros> Registros { get; set; }
+}
