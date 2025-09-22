7f152b049a527e1d8b8272405d152e532304e485
Blaiva <blayverthreyesp14@gmail.com>
2025-09-22 19:27:47 -0400

    Creacion del modelo Registros

---
+++ P1-AP1-BlayverthReyes/Models/Registros.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P1_AP1_BlayverthReyes.Models;
+
+public class Registros
+{
+    [Key]
+    public int RegistroId { get; set; }
+
+    [Required]
+    public string Nombres { get; set; }
+}
