a5be1dcd95aa27debf2c7b5af2126a4beffdffd2
Blaiva <blayverthreyesp14@gmail.com>
2025-09-29 18:19:14 -0400

    Actualizacion del modelo EntradasHuacales

--- P1-AP1-BlayverthReyes/Models/EntradasHuacales.cs
+++ P1-AP1-BlayverthReyes/Models/EntradasHuacales.cs
@@ -5,5 +5,20 @@ namespace P1_AP1_BlayverthReyes.Models;
 public class EntradasHuacales
 {
     [Key]
-    public int RegistroId { get; set; }
+    public int IdEntrada { get; set; }
+
+    [Required(ErrorMessage = "La fecha de entrada es requerida")]
+    public DateTime Fecha { get; set; }
+
+    [Required(ErrorMessage = "El nombre del cliente es requerido")]
+    [StringLength(50, ErrorMessage = "El tamaño del nombre debe ser menor a 50 caracteres")]
+    public string NombreCliente { get; set; }
+
+    [Required(ErrorMessage = "La cantidad de huacales es requerida")]
+    [Range(0, int.MaxValue, ErrorMessage = "La cantidad de huacales debe ser mayor que cero")]
+    public int Cantidad { get; set; }
+
+    [Required(ErrorMessage = "El precio de los huacales es requerido")]
+    [Range(0, int.MaxValue, ErrorMessage = "El precio de los huacales debe ser negativo")]
+    public double Precio { get; set; }
 }
