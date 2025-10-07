3e364eae53bccf014ff09998050bd342e066edcd
Blaiva <blayverthreyesp14@gmail.com>
2025-10-07 13:12:35 -0400

    Implemetacion de Toast y eliminacion de los Alert

--- P1-AP1-BlayverthReyes/Models/TiposHuacales.cs
+++ P1-AP1-BlayverthReyes/Models/TiposHuacales.cs
@@ -1,13 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace P1_AP1_BlayverthReyes.Models
-{
-    public class TiposHuacales
-    {
-        [Key]
-        public int TipoId { get; set; }
+namespace P1_AP1_BlayverthReyes.Models;
 
-        [Required]
-        public string Descripcion {  get; set; }
-    }
+public class TiposHuacales
+{
+    [Key]
+    public int TipoId { get; set; }
+
+    [Required]
+    public string Descripcion {  get; set; }
 }
