86fd8c5fe1baef561f4889cdefcd06f3d0f6e708
Blaiva <blayverthreyesp14@gmail.com>
2025-10-06 22:47:38 -0400

    Importacion de Bootstrap

---
+++ P1-AP1-BlayverthReyes/Models/TiposHuacales.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P1_AP1_BlayverthReyes.Models
+{
+    public class TiposHuacales
+    {
+        [Key]
+        public int TipoId { get; set; }
+
+        [Required]
+        public string Descripcion {  get; set; }
+    }
+}
